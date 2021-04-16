f1a6b7b4bd0fc8f8dd207d286024687a4aeb9bd5
Lucianna Tittle <lucianna.roset@gmail.com>
2021-04-16 18:06:50 -0500

    Finishing up components.

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,26 @@
 // Write your Character component here
-console.log("CodeGrade Submission");
+function Characters(props) {
+  const { charObj } = props;
+
+  return (
+    <div className="char-container">
+      <div className="char-info">
+        <p>{charObj.name}</p>
+
+        <p>Gender: {charObj.gender}</p>
+
+        <p>Height: {charObj.height}</p>
+
+        <p>Mass: {charObj.mass}</p>
+
+        <p>Birth Year: {charObj.birthYear}</p>
+
+        <p>Eye Color: {charObj.eyeColor}</p>
+
+        <p>Hair Color: {charObj.hairColor}</p>
+
+        <p>Skin Color: {charObj.skinColor}</p>
+      </div>
+    </div>
+  );
+}
